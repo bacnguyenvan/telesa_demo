2927bc72c2026fe44db413c5dd738e40ce37430e
bacDev <bacb4tvk@gmail.com>
2023-09-25 21:52:40 +0700

    add content

--- database/migrations/2023_09_25_203735_create_comment_detail_table.php
+++ database/migrations/2023_09_25_203735_create_comment_detail_table.php
@@ -19,6 +19,7 @@ class CreateCommentDetailTable extends Migration
             $table->integer('reply_id')->nullable();
             $table->integer('comment_id');
             $table->string('path', 255)->nullable();
+            $table->text('content')->nullable();
             $table->tinyInteger('type')->default(1)->comment('1: text, 2: file 3: audio, 4: video, 5: image');
             $table->datetime('created_time')->useCurrent();
             $table->timestamps();
