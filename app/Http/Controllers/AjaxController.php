ced72cab9ffb39a967953ee65c0351c8c11b3cbf
bacDev <bacb4tvk@gmail.com>
2023-10-18 22:14:20 +0700

    add queue low

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -124,7 +124,7 @@ class AjaxController extends Controller {
                     $fileConvert = pathinfo($filename, PATHINFO_FILENAME) . ".m3u8";
                     $dirConvert = $destinationPath . '/' . $fileConvert;
                     // $this->convertFile($dirConvert, $dir, $cd_id, $fileConvert);
-                    dispatch(new SendFileJob($dirConvert, $dir, $cd_id, $fileConvert));
+                    dispatch(new SendFileJob($dirConvert, $dir, $cd_id, $fileConvert))->onQueue("low");
                 }
 
                 // insert new comment - type upload file
